3293950f66ed938ffe3dba069e437e724908239a
Danny Sauer <github@dannysauer.com>
2020-10-27 16:54:26 -0500

    Shell script trivial cleanup
    * rewrap license/docs for 80 chars
    * Get rid of trailing spaces
    * Add missing quotes; remove extraneous quotes and braces
    * Alter argument check for fewer pipes (so much speed!)

--- contrib/faces/gravatar2card.sh
+++ contrib/faces/gravatar2card.sh
@@ -2,20 +2,20 @@
 
 # gravatar2card.sh
 # Copyright (C) 2015-2016 Jan-Piet Mens <jpmens@gmail.com>
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 # Usage: gravatar2card email "full name"
 #
@@ -29,7 +29,8 @@
 # mosquitto_pub -t owntracks/jane/phone/info -r -f card.json
 #
 # Note: the two commands cannot be piplelined (mosquitto_pub -l)
-# because of a bug in mosquitto_pub: https://bugs.eclipse.org/bugs/show_bug.cgi?id=478917
+# because of a bug in mosquitto_pub:
+#  https://bugs.eclipse.org/bugs/show_bug.cgi?id=478917
 # If you have a newer version it should work fine.
 
 
@@ -40,20 +41,19 @@ if [ -x /usr/bin/md5sum ]; then
 	md5=/usr/bin/md5sum
 fi
 
-email="$1"
-fullname="$2"
+email=$1
+fullname=$2
 tmp=$(mktemp /tmp/gravatar.XXXXXX)
 
-hash=$(echo -n "${email}" | tr '[:upper:]' '[:lower:]' | $md5)
+hash=$(echo -n "$email" | tr '[:upper:]' '[:lower:]' | "$md5")
 url="http://www.gravatar.com/avatar/${hash}?s=40"
 
+curl -s -o "$tmp" "$url"
 
-curl -s -o $tmp "${url}"
-
-imgdata=$(base64 -i $tmp)
-rm -f $tmp
+imgdata=$(base64 -i "$tmp")
+rm -f "$tmp"
 
 cat <<EndOfFile
-{"_type":"card","name":"${fullname}","face":"${imgdata}"}
+{"_type":"card","name":"$fullname","face":"$imgdata"}
 EndOfFile
 
--- contrib/faces/image2card.sh
+++ contrib/faces/image2card.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-# image2card.sh 
+# image2card.sh
 # Copyright (C) 2015-2016 Jan-Piet Mens <jpmens@gmail.com>
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 # Usage: image2card imagefile "full name"
 #
@@ -30,20 +30,22 @@
 # mosquitto_pub -t owntracks/jane/phone/info -f card.json -r
 #
 # Note: the two commands cannot be piplelined (mosquitto_pub -l)
-# because of a bug in mosquitto_pub: https://bugs.eclipse.org/bugs/show_bug.cgi?id=478917
+# because of a bug in mosquitto_pub:
+#  https://bugs.eclipse.org/bugs/show_bug.cgi?id=478917
 # If you have a newish version it should work fine.
 
 
 [ $# -ne 2 ] && { echo "Usage: $0 image-file full-name" >&2; exit 2; }
-imagefile="$1"
-fullname="$2"
+imagefile=$1
+fullname=$2
 
 base64switch=""
-base64check=$(echo "jj" | base64 -w 0 > /dev/null 2>&1)
-[ "$?" -eq "0" ] && base64switch="-w 0"
-imgdata=$(convert "${imagefile}" -resize '40x40!' - | base64 $base64switch)
+if base64 -w 0 <"$0" >/dev/null 2>&1; then
+	base64switch="-w 0"
+fi
+imgdata=$(convert "$imagefile" -resize '40x40!' - | base64 $base64switch)
 
 cat <<EndOfFile
-{"_type":"card","name":"${fullname}","face":"${imgdata}"}
+{"_type":"card","name":"$fullname","face":"$imgdata"}
 EndOfFile
 
